a9e3c641bb7568f722efc0e32a66dc98d1a9a5e3
Daniel Hamill <hamill.daniel@gmail.com>
2022-06-06 11:15:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,20 @@ setuptools.setup(
  author = 'Daniel Hamill',
  author_email = 'daniel.hamill@hey.com',
  description = 'Parse HEC-SSP report Files',
- packages = ['parsehecssp', 'parsehecssp.volumeFrequency']
+ packages = ['parsehecssp', 'parsehecssp.volumeFrequency'],
+ license='MIT',
+ package_dir={'':'parsehecssp'},
+ classifiers=[
+    "Development Status :: 1 - Beta",
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    "License :: OSI Approved :: MIT License",
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    "Topic :: Utilities",
+    'Topic :: Scientific/Engineering :: GIS',
+]
+ 
 )
