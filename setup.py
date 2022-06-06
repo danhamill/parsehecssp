3459fa7db263026451964eb1d84afe53e795423d
Daniel Hamill <hamill.daniel@gmail.com>
2022-06-06 11:02:05 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+ name='parsehecssp',
+ version = '0.0.1',
+ author = 'Daniel Hamill',
+ author_email = 'daniel.hamill@hey.com',
+ description = 'Parse HEC-SSP report Files',
+ packages = ['parsehecssp', 'parsehecssp.volumeFrequency']
+)
