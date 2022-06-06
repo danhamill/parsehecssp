a858d83fe442fa13e4c171fae3900088438fbca8
danhamill <hamill.daniel@gmail.com>
2022-06-06 12:00:36 -0700

    long desc

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
 
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
 setuptools.setup(
  name='parsehecssp',
  version = '0.0.1',
  author = 'Daniel Hamill',
  author_email = 'daniel.hamill@hey.com',
  description = 'Parse HEC-SSP report Files',
+ long_description=readme,
  packages = ['parsehecssp', 'parsehecssp.features'],
  license='MIT',
 #  package_dir={'':'parsehecssp'},
